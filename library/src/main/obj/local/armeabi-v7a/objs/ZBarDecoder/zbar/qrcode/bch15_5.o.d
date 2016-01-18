23541a581691968cb31ba80ef9483ce02841add0
xxl6097 <263996097@qq.com>
2016-01-18 20:30:24 +0800

    delete obj cache file

--- library/src/main/obj/local/armeabi-v7a/objs/ZBarDecoder/zbar/qrcode/bch15_5.o.d
+++
@@ -1,5 +0,0 @@
-E:/HeT/studioproject/ZbarScanLibrary/library/src/main/obj/local/armeabi-v7a/objs/ZBarDecoder/zbar/qrcode/bch15_5.o: \
- E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/qrcode/bch15_5.c \
- E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/qrcode/bch15_5.h
-
-E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/qrcode/bch15_5.h:
--- library/src/main/obj/local/x86/objs/iconv/libiconv-1.14/lib/relocatable.o.d
+++
@@ -1,8 +0,0 @@
-E:/HeT/studioproject/ZbarScanLibrary/library/src/main/obj/local/x86/objs/iconv/libiconv-1.14/lib/relocatable.o: \
- E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/libiconv-1.14/lib/relocatable.c \
- E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/libiconv-1.14/libcharset/config.h \
- E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/libiconv-1.14/lib/relocatable.h
-
-E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/libiconv-1.14/libcharset/config.h:
-
-E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/libiconv-1.14/lib/relocatable.h:
