1160b8a74917344addf0166613b4ba721e78fefb
xxl6097 <263996097@qq.com>
2016-01-18 20:17:30 +0800

    add zbar c and c++,and read chinese aaaaaaa

---
+++ library/src/main/obj/local/x86/objs/ZBarDecoder/zbar/qrcode/qrdectxt.o.d
@@ -0,0 +1,203 @@
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/obj/local/x86/objs/ZBarDecoder/zbar/qrcode/qrdectxt.o: \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/qrcode/qrdectxt.c \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/config.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/stdio.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/android/api-level.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/types.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/stdint.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/_wchar_limits.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/types.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/types.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/stdlib.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/string.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/malloc.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/alloca.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/strings.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/memory.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/libiconv-1.14/include/iconv.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/errno.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/errno.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/errno.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm-generic/errno.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm-generic/errno-base.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/wchar.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/limits.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/limits.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/limits.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/machine/internal_types.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/machine/limits.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/syslimits.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/page.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/page_32.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/time.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/time.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/time.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/siginfo.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm-generic/siginfo.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/qrcode.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/include/zbar.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/qrcode/qrdec.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/qrcode/util.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/image.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/inttypes.h \
+ D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/assert.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/error.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/symbol.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/refcnt.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/debug.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/ean.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/i25.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/databar.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/codabar.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/code39.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/code93.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/code128.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/qr_finder.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/error.h \
+ E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/img_scanner.h
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/config.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/stdio.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/android/api-level.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/types.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/_wchar_limits.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/types.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/types.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/stdlib.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/string.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/malloc.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/alloca.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/strings.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/memory.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/libiconv-1.14/include/iconv.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/errno.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/errno.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/errno.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm-generic/errno.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm-generic/errno-base.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/wchar.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/limits.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/limits.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/limits.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/machine/internal_types.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/machine/limits.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/syslimits.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/page.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/page_32.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/time.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/sys/time.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/linux/time.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm/siginfo.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/asm-generic/siginfo.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/qrcode.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/include/zbar.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/qrcode/qrdec.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/qrcode/util.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/image.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/inttypes.h:
+
+D:/Android/android-sdk_r24.3.4-windows/android-sdk-windows/ndk-bundle/platforms/android-9/arch-x86/usr/include/assert.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/error.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/symbol.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/refcnt.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/debug.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/ean.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/i25.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/databar.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/codabar.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/code39.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/code93.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/code128.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/decoder/qr_finder.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/error.h:
+
+E:/HeT/studioproject/ZbarScanLibrary/library/src/main/jni/zbar/img_scanner.h:
