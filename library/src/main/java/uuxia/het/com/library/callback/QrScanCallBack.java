85df9cbf90f9751212e4f92e255b355e215432b9
xxl6097 <263996097@qq.com>
2016-01-20 17:09:10 +0800

    delete and add the third

--- library/src/main/java/uuxia/het/com/library/callback/QrScanCallBack.java
+++
@@ -1,9 +0,0 @@
-package uuxia.het.com.library.callback;
-
-/**
- * Created by uuxia-mac on 16/1/17.
- */
-public interface QrScanCallBack {
-    void onResult(String resuilt);
-    void onException(Object object,Exception e);
-}
